1924b4b7b951b3df423e32d7bb1842c6b263bb30
@NurtureLearning <121309018+YesInAJiffy@users.noreply.github.com>
2025-08-02 13:37:18 -0400

    Create table.py

---
+++ commit/table.py
@@ -0,0 +1,4 @@
+for i in range(2, 21):  # Tables from 2 to 20
+    print(f"\nMultiplication Table for {i}")
+    for j in range(1, 11):  # Each table from 1 to 10
+        print(f"{i} x {j} = {i * j}")
