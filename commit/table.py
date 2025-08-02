b97b6c2a719824cb529bf5e0c4c53ba0aec9e7f4
@NurtureLearning <121309018+YesInAJiffy@users.noreply.github.com>
2025-08-02 18:41:19 -0400

    Update table.py

--- commit/table.py
+++ commit/table.py
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
-# Tables
-=======
-# My Tables Printing
-# Tables Printing
->>>>>>> a7e98535740e0dd21721113aa9bcc17043928b67
+#Checking for Rebase, changes done directly at server.
 for i in range(2, 21):  # Tables from 2 to 20
     print(f"\nMultiplication Table for {i}")
     for j in range(1, 11):  # Each table from 1 to 10
