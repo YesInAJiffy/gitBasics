59279bba41cdc93facb91d9035f807d8d5fdd92f
John Doe <JohnDoe@nurturelearning.com>
2025-08-02 22:47:31 -0400

    no message

--- commit/table.py
+++ commit/table.py
@@ -1,4 +1,5 @@
 #Checking for Rebase, changes done directly at server.
+# Second Change done now
 for i in range(2, 21):  # Tables from 2 to 20
     print(f"\nMultiplication Table for {i}")
     for j in range(1, 11):  # Each table from 1 to 10
