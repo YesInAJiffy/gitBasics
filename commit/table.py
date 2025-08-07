0dac0240651d0993b740ed25473612ed714a4072
@NurtureLearning <121309018+YesInAJiffy@users.noreply.github.com>
2025-08-07 12:29:43 -0400

    Update table.py

--- commit/table.py
+++ commit/table.py
@@ -4,3 +4,4 @@ for i in range(2, 21):  # Tables from 2 to 20
     print(f"\nMultiplication Table for {i}")
     for j in range(1, 11):  # Each table from 1 to 10
         print(f"{i} x {j} = {i * j}")
+# Added this on 7th August at 12:29 PM
